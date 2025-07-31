e9c1768e5bef810d35a95b5ee5ccdd2c61a248e7
davitavares777 <davi.fernando.tavares@escola.pr.gov.br>
2025-07-31 08:47:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+ function multiplicação(){
+
+    let C = parseInt(document.getElementById('capital').value);
+    let T = parseInt(document.getElementById('tempo').value);
+    let I = parseInt(document.getElementById('porcentagem').value);
+
+    let multiplicação = C * T * I
+
+    document.getElementById('resultado').textContent = 'Resultado: '+ multiplicação;
+
+
+ }
+ M = J + C 
+ function soma(){
+
+    let J = parseInt(document.getElementById('primeiro_numero').value);
+    let i = parseInt(document.getElementById('segundo_numero').value);
+
+    let soma = numero_um + numero_dois;
+
+    document.getElementById('resultado do montante final').textContent = 'resultado do montante final: '+ soma;
+
+   
+}
+
+
+
+
+
+
+
